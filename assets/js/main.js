8b414d8980702acb45efa4a6ce06cdaff1d2e4eb
ChristianAglanu <christianaglanu14@gmail.com>
2026-04-17 09:15:28 +0000

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,6 +0,0 @@
-function downloadResume() {
-    const link = document.createElement('a');
-    link.href = 'assets/resume.pdf';
-    link.download = 'Christian-Aglanu-Resume.pdf';
-    link.click();
-}
